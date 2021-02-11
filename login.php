146264a14b2590e7b00318e6ecbeef3d3816df40
AngelinaGomes <gmsangelina@gmail.com>
2021-02-11 00:55:30 -0300

    UpdateLogin

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ $username = mysqli_real_escape_string($connection, $_POST['username']);
 $password = mysqli_real_escape_string($connection, $_POST['senha']);
 
 if (($username && $password)) {
-    $query = "SELECT ID, Login, TipoUsuarioID FROM `Funcionario` WHERE Login = '{$username}' AND Senha = md5('{$password}')";
+    $query = "SELECT Cpf, Login, TipoUsuarioId FROM `Usuario` WHERE Login = '{$username}' AND Senha = md5('{$password}')";
     echo $query;
     $result = mysqli_query($connection, $query);
     $row = mysqli_num_rows($result);
@@ -14,7 +14,7 @@ if (($username && $password)) {
 
     if($row==1){
         $_SESSION['usuario'] = $username;
-        $_SESSION['tipoUserId'] = $data['TipoUsuarioID'];
+        $_SESSION['tipoUserId'] = $data['TipoUsuarioId'];
         header('location: home.php');
         exit();
     }else{
@@ -26,3 +26,5 @@ if (($username && $password)) {
 } else {
     echo '<script>alert("Por favor, adicione Login e Senha"); location="index.php"; </script>';
 }
+
+?>
