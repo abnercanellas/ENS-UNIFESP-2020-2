5eb341300356c01daf9f93a55517a76c3e1f5122
AngelinaGomes <gmsangelina@gmail.com>
2021-02-11 00:39:05 -0300

    EditDatabase

--- sql/createTableQuery.sql
+++ sql/createTableQuery.sql
@@ -1,65 +1,112 @@
+CREATE DATABASE insertion;
+USE insertion;
+
+CREATE TABLE Vinculo(
+Id integer(10) AUTO_INCREMENT,
+Vinc varchar(255) NOT NULL UNIQUE,
+PRIMARY KEY (Id));
+
+CREATE TABLE Categoria(
+Id integer(10) AUTO_INCREMENT,
+Cat varchar(255) NOT NULL UNIQUE,
+Sigla varchar(10) NOT NULL UNIQUE,
+PRIMARY KEY (Id));
+
 CREATE TABLE TipoUsuario(
-ID integer(1) NOT NULL AUTO_INCREMENT,
-Tipo varchar(255) NOT NULL,
-PRIMARY KEY (ID));
+Id integer(10) AUTO_INCREMENT,
+Tipo varchar(255) NOT NULL UNIQUE,
+Acesso integer(10) NOT NULL,
+PRIMARY KEY (Id));
+
+CREATE TABLE Condicao(
+Id integer(10) AUTO_INCREMENT,
+Cond varchar(255) NOT NULL UNIQUE,
+PRIMARY KEY (Id));
+/*
+** Ativo
+** Licença
+** Férias
+*/
+
+CREATE TABLE Bloco(
+Id integer(10) AUTO_INCREMENT,
+Nome varchar(255) NOT NULL UNIQUE,
+PRIMARY KEY (Id));
 
 CREATE TABLE Setor(
-ID integer(1),
-Nome varchar(255) NOT NULL,
-PRIMARY KEY (ID));
-
-CREATE TABLE TipoFerias(
-ID integer(1) NOT NULL AUTO_INCREMENT,
-Tipo varchar(255) NOT NULL,
-PRIMARY KEY (ID));
+Id integer(10) AUTO_INCREMENT,
+Nome varchar(255) NOT NULL UNIQUE,
+BlocoId integer(10) NOT NULL,
+PRIMARY KEY (Id),
+FOREIGN KEY (BlocoId) REFERENCES Bloco (Id));
 
 CREATE TABLE TipoEscala(
-ID integer(1) NOT NULL AUTO_INCREMENT,
-TipoEscala varchar(255) NOT NULL,
-PRIMARY KEY (ID));
+Id integer(10) AUTO_INCREMENT,
+Tipo varchar(255) NOT NULL UNIQUE,
+Dias integer(2) NOT NULL,
+Horas integer(2) NOT NULL,
+PRIMARY KEY (Id));
+/*
+** Dias: número de dias seguidos de trabalho
+** Horas: quantidade de horas trabalhadas por dia
+** Exemplos:
+**    Tipo: 6x1 6, Dias: 6, Horas: 6
+**    Tipo: 6x1 8, Dias: 6, Horas: 8
+**    Tipo: 12x36, Dias: 1, Horas: 12
+*/
 
-CREATE TABLE Funcionario(
-ID integer(1),
-Nome varchar(255),
-Login integer(1),
-Senha varchar(30),
-TipoUsuarioID integer(1),
-SetorID integer(1),
-TipoEscalaID integer(1),
-TipoFeriasID integer(1),
-PRIMARY KEY (ID),
-FOREIGN KEY (TipoUsuarioID) REFERENCES TipoUsuario (ID),
-FOREIGN KEY (SetorID) REFERENCES Setor (ID),
-FOREIGN KEY (TipoEscalaID) REFERENCES TipoEscala (ID),
-FOREIGN KEY (TipoFeriasID) REFERENCES TipoFerias (ID));
+CREATE TABLE TipoFerias(
+Id integer(10) AUTO_INCREMENT,
+Nome varchar(255) NOT NULL UNIQUE,
+Duracao integer(2) NOT NULL UNIQUE, /*número de dias*/
+PRIMARY KEY (Id));
+
+CREATE TABLE Usuario(
+Cpf varchar(11),
+RfRe integer(10) NOT NULL UNIQUE,
+Coren integer(10) NOT NULL UNIQUE,
+Nome varchar(255) NOT NULL,
+Login varchar(11) NOT NULL UNIQUE,
+Senha varchar(32) NOT NULL,
+VinculoId integer(10) NOT NULL,
+CategoriaId integer(10) NOT NULL,
+TipoUsuarioId integer(10) NOT NULL,
+CondicaoId integer(10) NOT NULL,
+SetorId integer(10) NOT NULL,
+TipoEscalaId integer(10) NOT NULL,
+TipoFeriasId integer(10) NOT NULL,
+PRIMARY KEY (Cpf),
+FOREIGN KEY (VinculoId) REFERENCES Vinculo (Id),
+FOREIGN KEY (CategoriaId) REFERENCES Categoria (Id),
+FOREIGN KEY (TipoUsuarioId) REFERENCES TipoUsuario (Id),
+FOREIGN KEY (CondicaoId) REFERENCES Condicao (Id),
+FOREIGN KEY (SetorId) REFERENCES Setor (Id),
+FOREIGN KEY (TipoEscalaId) REFERENCES TipoEscala (Id),
+FOREIGN KEY (TipoFeriasId) REFERENCES TipoFerias (Id));
 
 CREATE TABLE TipoPresenca(
-ID integer(1),
-Tipo varchar(255) NOT NULL,
-PRIMARY KEY (ID));
+Id integer(10) AUTO_INCREMENT,
+Sigla varchar(5) NOT NULL UNIQUE,
+Tipo varchar(255) NOT NULL UNIQUE,
+PRIMARY KEY (Id));
 
 CREATE TABLE Escalas(
-ID integer(1),
-TipoEscalaID integer(1),
-PRIMARY KEY (ID),
-FOREIGN KEY (TipoEscalaID) REFERENCES TipoEscala (ID));
+Id integer(10) AUTO_INCREMENT,
+DataInicio date,
+DataFim date,
+TipoEscalaId integer(10),
+PRIMARY KEY (Id),
+FOREIGN KEY (TipoEscalaId) REFERENCES TipoEscala (Id));
 
 CREATE TABLE Celula(
-ID integer(1),
+Id integer(10) AUTO_INCREMENT,
 DataC date,
-TipoPresencaID integer(1),
-EscalasID integer(1),
-FuncionarioID integer(1),
-PRIMARY KEY (ID),
-FOREIGN KEY (TipoPresencaID) REFERENCES TipoPresenca (ID),
-FOREIGN KEY (EscalasID) REFERENCES Escalas (ID),
-FOREIGN KEY (FuncionarioID) REFERENCES Funcionario (ID));
-
-
-ALTER TABLE Funcionario MODIFY Nome varchar(255) NOT NULL;
-ALTER TABLE Funcionario MODIFY Login varchar(30) NOT NULL;
-ALTER TABLE Funcionario MODIFY Senha varchar(32) NOT NULL;
-ALTER TABLE Funcionario MODIFY TipoUsuarioID integer(1) NOT NULL;
-ALTER TABLE Funcionario MODIFY SetorID integer(1) NOT NULL;
-ALTER TABLE Funcionario MODIFY TipoEscalaID integer(1) NOT NULL;
-ALTER TABLE Funcionario MODIFY TipoFeriasID integer(1) NOT NULL;
+HoraInicio time(6),
+HoraFim time(6),
+TipoPresencaId integer(10),
+EscalasId integer(10),
+UsuarioId varchar(11),
+PRIMARY KEY (Id),
+FOREIGN KEY (TipoPresencaId) REFERENCES TipoPresenca (Id),
+FOREIGN KEY (EscalasId) REFERENCES Escalas (Id),
+FOREIGN KEY (UsuarioId) REFERENCES Usuario (Cpf));
--- sql/insertQuery.sql
+++ sql/insertQuery.sql
@@ -1,46 +1,78 @@
-INSERT INTO TipoUsuario (Tipo)
-VALUES ('Diretoria');
+USE insertion;
 
-INSERT INTO TipoUsuario (Tipo)
-VALUES ('Supervisao');
+INSERT INTO Vinculo (Vinc) VALUES ('SPDM');
 
-INSERT INTO TipoUsuario (Tipo)
-VALUES ('Funcionario');
+INSERT INTO Categoria (Cat, Sigla) VALUES ('Enfermeiro(a)', 'ENF');
+INSERT INTO Categoria (Cat, Sigla) VALUES ('Enfermeiro(a) Pleno', 'ENF PLENO');
+INSERT INTO Categoria (Cat, Sigla) VALUES ('Auxiliar de Enfermagem', 'AE');
+INSERT INTO Categoria (Cat, Sigla) VALUES ('Técnico de Enfermagem', 'TE');
+INSERT INTO Categoria (Cat, Sigla) VALUES ('Enfermeiro Assistente', 'ENF A');
+INSERT INTO Categoria (Cat, Sigla) VALUES ('Auxiliar de Enfermagem Junior', 'AE JR');
+
+INSERT INTO TipoUsuario (Tipo, Acesso) VALUES ('Diretoria de enfermagem', 1);
+INSERT INTO TipoUsuario (Tipo, Acesso) VALUES ('Coordenação técnica', 2);
+INSERT INTO TipoUsuario (Tipo, Acesso) VALUES ('Supervisão de enfermagem', 2);
+INSERT INTO TipoUsuario (Tipo, Acesso) VALUES ('Padrão', 3);
+INSERT INTO TipoUsuario (Tipo, Acesso) VALUES ('Folguista', 3);
+INSERT INTO TipoUsuario (Tipo, Acesso) VALUES ('Ferista', 3);
+
+INSERT INTO Condicao (Cond) VALUES ('Ativo');
+INSERT INTO Condicao (Cond) VALUES ('Licença');
+INSERT INTO Condicao (Cond) VALUES ('Férias');
+
+INSERT INTO Bloco (Nome) VALUES ('Clínicas');
+INSERT INTO Bloco (Nome) VALUES ('Materno Infantil');
+INSERT INTO Bloco (Nome) VALUES ('Pediátrico');
+INSERT INTO Bloco (Nome) VALUES ('UTIS Adulto');
+INSERT INTO Bloco (Nome) VALUES ('Pronto Socorro Adulto');
+
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Clínica Médica', 1);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Clínica Cirúrgica II', 1);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Clínica Cirúrgica III', 1);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Clínica Cirúrgica IV', 1);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Centro Obstétrico', 2);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Maternidade', 2);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Hospital da Mulher', 2);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('UTI Neonatal', 2);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Pediatria', 3);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Pronto Socorro Infantil', 3);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('UTI Infantil', 3);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('UTI Adulto I', 4);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('UTI Adulto II', 4);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('UTI PS', 4);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Classificação de Risco', 5);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Vermelho', 5);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Amarelo', 5);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Verde', 5);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Acolhimento', 5);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Protocolo Analgesia', 5);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('ECG', 5);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Curativo / Sutura', 5);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Tomografia e Raio X', 5);
+INSERT INTO Setor (Nome, BlocoId) VALUES ('Ortopedia', 5);
+
+INSERT INTO TipoEscala (Tipo, Dias, Horas) VALUES ('6x1 8h', 6, 8);
+INSERT INTO TipoEscala (Tipo, Dias, Horas) VALUES ('6x1 6h', 6, 6);
+INSERT INTO TipoEscala (Tipo, Dias, Horas) VALUES ('12x36', 1, 12);
+
+INSERT INTO TipoFerias (Nome, Duracao) VALUES ('Integral', 30);
+INSERT INTO TipoFerias (Nome, Duracao) VALUES ('Metade', 15);
 
 
-INSERT INTO Setor (ID, Nome)
-VALUES ('331', 'Clinica Cirurgica I');
+INSERT INTO Usuario (Cpf, RfRe, Coren, Nome, Login, Senha, VinculoId, CategoriaId, TipoUsuarioId, CondicaoId, SetorId, TipoEscalaId, TipoFeriasId)
+VALUES ('11122233344', 1234567, 1112223, 'Maria Alice Silva', '11122233344', MD5('123456'), 1, 2, 1, 1, 1, 1, 1);
 
-INSERT INTO Setor (ID, Nome)
-VALUES ('332', 'Clinica Cirurgica II');
+INSERT INTO Usuario (Cpf, RfRe, Coren, Nome, Login, Senha, VinculoId, CategoriaId, TipoUsuarioId, CondicaoId, SetorId, TipoEscalaId, TipoFeriasId)
+VALUES ('22233344455', 2345678, 2223334, 'Carlos Ferreira', '22233344455', MD5('123456'), 1, 1, 3, 1, 2, 3, 1);
 
-INSERT INTO Setor (ID, Nome)
-VALUES ('773', 'UTI');
-
-INSERT INTO Setor (ID, Nome)
-VALUES ('126', 'Administracao');
+INSERT INTO Usuario (Cpf, RfRe, Coren, Nome, Login, Senha, VinculoId, CategoriaId, TipoUsuarioId, CondicaoId, SetorId, TipoEscalaId, TipoFeriasId)
+VALUES ('33344455566', 3456789, 3334445, 'Geovana Nascimento', '33344455566', MD5('123456'), 1, 1, 4, 1, 2, 3, 2);
 
 
-INSERT INTO TipoEscala (TipoEscala)
-VALUES ('8h manha');
-
-INSERT INTO TipoEscala (TipoEscala)
-VALUES ('8h tarde');
-
-
-INSERT INTO TipoFerias (Tipo)
-VALUES ('30 dias');
-
-INSERT INTO TipoFerias (Tipo)
-VALUES ('15 dias');
-
-
-INSERT INTO Funcionario (ID, Nome, Login, Senha, TipoUsuarioID, SetorID, TipoEscalaID, TipoFeriasID)
-VALUES ('1234', 'Maria Alice Silva', 'marialices', MD5('123456'), '1', '126', '1', '1');
-
-INSERT INTO Funcionario (ID, Nome, Login, Senha, TipoUsuarioID, SetorID, TipoEscalaID, TipoFeriasID)
-VALUES ('2345', 'Carlos Pereira', 'carlospereira', MD5('123456'), '2', '126', '1', '2');
-
-INSERT INTO Funcionario (ID, Nome, Login, Senha, TipoUsuarioID, SetorID, TipoEscalaID, TipoFeriasID)
-VALUES ('3456', 'Geovana Nascimento', 'geovananasc', MD5('123456'), '3', '773', '2', '1');
-
+INSERT INTO TipoPresenca (Sigla, Tipo) VALUES ('X', 'Presente');
+INSERT INTO TipoPresenca (Sigla, Tipo) VALUES ('F', 'Folga');
+INSERT INTO TipoPresenca (Sigla, Tipo) VALUES ('FF', 'Folga Feriado');
+INSERT INTO TipoPresenca (Sigla, Tipo) VALUES ('FE', 'Folga Eleitoral');
+INSERT INTO TipoPresenca (Sigla, Tipo) VALUES ('FP', 'Folga Prêmio');
+INSERT INTO TipoPresenca (Sigla, Tipo) VALUES ('A', 'Ausência');
+INSERT INTO TipoPresenca (Sigla, Tipo) VALUES ('AT', 'Ausência com Atestado');
