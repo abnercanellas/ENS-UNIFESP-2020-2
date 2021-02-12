5cf5818136febc52e6999b150ba3a5343fe8d191
AngelinaGomes <gmsangelina@gmail.com>
2021-02-12 03:51:23 -0300

    EditQueryFiles

--- attFunc.php
+++ attFunc.php
@@ -10,11 +10,14 @@ function gather($connection,$query){
     $s=mysqli_query($connection,$query);
     if(mysqli_num_rows($s)>0){
         while($r = mysqli_fetch_array($s)){
-            $tipoU = mysqli_fetch_array(mysqli_query($connection,"SELECT Tipo  FROM `tipousuario` WHERE `ID` = {$r['TipoUsuarioID']}"))['Tipo'];
-            $tipoS = mysqli_fetch_array(mysqli_query($connection,"SELECT Nome  FROM `setor` WHERE `ID` = {$r['SetorID']}"))['Nome'];
-            $tipoE = mysqli_fetch_array(mysqli_query($connection,"SELECT TipoEscala  FROM `tipoescala` WHERE `ID` = {$r['TipoEscalaID']}"))['TipoEscala'];
-            $tipoF = mysqli_fetch_array(mysqli_query($connection,"SELECT Tipo  FROM `tipoferias` WHERE `ID` = {$r['TipoFeriasID']}"))['Tipo'];
-            echo '<p>Nome: '.$r['Nome'].' <br> -  CPF/COREN: '.$r['ID'].'<br> -  Nivel de acesso: '.$tipoU.'<br> -  Setor: '.$tipoS.'<br> -  Escala: '.$tipoE.'<br> -  Férias: '.$tipoF.'.</p>';
+            $vinc  = mysqli_fetch_array(mysqli_query($connection,"SELECT Vinc  FROM `Vinculo`     WHERE `Id` = {$r['VinculoId']}"))['Vinc'];
+            $cat   = mysqli_fetch_array(mysqli_query($connection,"SELECT Cat   FROM `Categoria`   WHERE `Id` = {$r['CategoriaId']}"))['Cat'];
+            $tipoU = mysqli_fetch_array(mysqli_query($connection,"SELECT Tipo  FROM `TipoUsuario` WHERE `Id` = {$r['TipoUsuarioId']}"))['Tipo'];
+            $cond  = mysqli_fetch_array(mysqli_query($connection,"SELECT Cond  FROM `Condicao`    WHERE `Id` = {$r['CondicaoId']}"))['Cond'];
+            $tipoS = mysqli_fetch_array(mysqli_query($connection,"SELECT Nome  FROM `Setor`       WHERE `Id` = {$r['SetorId']}"))['Nome'];
+            $tipoE = mysqli_fetch_array(mysqli_query($connection,"SELECT Tipo  FROM `TipoEscala`  WHERE `Id` = {$r['TipoEscalaId']}"))['Tipo'];
+            $tipoF = mysqli_fetch_array(mysqli_query($connection,"SELECT Nome  FROM `TipoFerias`  WHERE `Id` = {$r['TipoFeriasId']}"))['Nome'];
+            echo '<p>Nome: '.$r['Nome'].' <br> -  CPF(CRNM): '.$r['Cpf'].'<br> -  RF/RE: '.$r['RfRe'].'<br> -  COREN: '.$r['Coren'].'<br> -  Vínculo: '.$vinc.'<br> -  Categoria: '.$cat.'<br> -  Tipo de usuário: '.$tipoU.'<br> -  Condição: '.$cond.'<br> -  Setor: '.$tipoS.'<br> -  Escala: '.$tipoE.'<br> -  Férias: '.$tipoF.'.</p>';
         }
     }else{
         echo "nenhuma correspondencia encontrada";
@@ -40,27 +43,75 @@ function gather($connection,$query){
             if(isset($_POST['formConsulta'])){
             $TipoCadastro = mysqli_real_escape_string($connection, $_POST['sTipoConsulta']);
                 switch ($TipoCadastro) {
-                    case '0': //Funcionario
+                    case '0': //Nome
                         $n = mysqli_real_escape_string($connection, $_POST['iNomeUsuario']);
-                        $query = "SELECT * FROM `Funcionario` WHERE `Nome` LIKE '%{$n}%'";
+                        $query = "SELECT * FROM `Usuario` WHERE `Nome` LIKE '%{$n}%'";
                         gather($connection,$query);                        
                         break;
                 
-                    case '1': //TipoUsuario
+                    case '1': //CPF
                         $n = mysqli_real_escape_string($connection, $_POST['iCpf']);
-                        $query = "SELECT * FROM `Funcionario` WHERE `ID` LIKE '%{$n}%'";
+                        $query = "SELECT * FROM `Usuario` WHERE `Cpf` LIKE '%{$n}%'";
                         gather($connection,$query); 
                         break;
-                
-                    case '2': //Setor
+                    
+                    case '2': //RF/RE
+                        $n = mysqli_real_escape_string($connection, $_POST['iRfRe']);
+                        $query = "SELECT * FROM `Usuario` WHERE `RfRe` LIKE '%{$n}%'";
+                        gather($connection,$query); 
+                        break;
+                    
+                    case '3': //COREN
+                        $n = mysqli_real_escape_string($connection, $_POST['iCoren']);
+                        $query = "SELECT * FROM `Usuario` WHERE `Coren` LIKE '%{$n}%'";
+                        gather($connection,$query); 
+                        break;
+
+                    case '4': //Vínculo
+                        $n = mysqli_real_escape_string($connection, $_POST['sVinculo']);
+                        $query = "SELECT * FROM `Usuario` WHERE `VinculoId` LIKE '%{$n}%'";
+                        gather($connection,$query); 
+                        break;
+
+                    case '5': //Categoria
+                        $n = mysqli_real_escape_string($connection, $_POST['sCategoria']);
+                        $query = "SELECT * FROM `Usuario` WHERE `CategoriaId` LIKE '%{$n}%'";
+                        gather($connection,$query); 
+                        break;
+                        
+                    case '6': //TipoUsuario
                         $n = mysqli_real_escape_string($connection, $_POST['sTipoUsuario']);
-                        $query = "SELECT * FROM `Funcionario` WHERE `TipoUsuarioID` LIKE '%{$n}%'";
+                        $query = "SELECT * FROM `Usuario` WHERE `TipoUsuarioId` LIKE '%{$n}%'";
+                        gather($connection,$query); 
+                        break;
+
+                    case '7': //Condição
+                        $n = mysqli_real_escape_string($connection, $_POST['sCondicao']);
+                        $query = "SELECT * FROM `Usuario` WHERE `CondicaoId` LIKE '%{$n}%'";
                         gather($connection,$query); 
                         break;
                 
-                    case '3': //TipoPresenca
+                    case '8': //Nível de Acesso
+                        $n = mysqli_real_escape_string($connection, $_POST['sAcesso']);
+                        $query = "SELECT * FROM `Usuario` WHERE `TipoUsuarioId` IN (SELECT `Id` FROM `TipoUsuario` WHERE `Acesso` LIKE '%{$n}%')";
+                        gather($connection,$query); 
+                        break;
+
+                    case '9': //Bloco
+                        $n = mysqli_real_escape_string($connection, $_POST['sBloco']);
+                        $query = "SELECT * FROM `Usuario` WHERE `SetorId` IN (SELECT `Id` FROM `Setor` WHERE `BlocoId` LIKE '%{$n}%')";
+                        gather($connection,$query); 
+                        break;
+                        
+                    case '10': //Setor
                         $n = mysqli_real_escape_string($connection, $_POST['sSetor']);
-                        $query = "SELECT * FROM `Funcionario` WHERE `SetorID` LIKE '%{$n}%'";
+                        $query = "SELECT * FROM `Usuario` WHERE `SetorId` LIKE '%{$n}%'";
+                        gather($connection,$query); 
+                        break;
+
+                    case '11': //Tipo de Escala
+                        $n = mysqli_real_escape_string($connection, $_POST['sTipoEscala']);
+                        $query = "SELECT * FROM `Usuario` WHERE `TipoEscalaId` LIKE '%{$n}%'";
                         gather($connection,$query); 
                         break;
                 }
--- consulta.php
+++ consulta.php
@@ -33,13 +33,21 @@
                     <form class="m-0" action="" method="POST">
                             <legend>Consulta: </legend>
                             
-                            <div id="tipoConsulta"> <!-- escolhe o tipo de cadastro a realizar -->
+                            <div id="tipoConsulta"> <!-- escolhe o tipo de consulta a realizar -->
                                 <select class="form-select my-3" name="sTipoConsulta" id="idsTipoConsulta" onchange="selectCheck()">
                                     <option selected>Selecione aqui</option>
                                     <option value="0">Nome</option>
-                                    <option value="1">CPF/COREN</option>
-                                    <option value="2">Nível</option>
-                                    <option value="3">Setor</option>
+                                    <option value="1">CPF</option>
+                                    <option value="2">RF/RE</option>
+                                    <option value="3">COREN</option>
+                                    <option value="4">Vínculo</option>
+                                    <option value="5">Categoria</option>
+                                    <option value="6">Tipo de Usuário</option>
+                                    <option value="7">Condição</option>
+                                    <option value="8">Nível de Acesso</option>
+                                    <option value="9">Bloco</option>
+                                    <option value="10">Setor</option>
+                                    <option value="11">Tipo de Escala</option>
                                 </select>
                             </div>
 
@@ -47,7 +55,7 @@
                                 <hr class="mb-5">           
                                 <div class="input-group mb-3">
                                     <label class="input-group-text"  for="iNomeUsuario">Nome Completo: </label>
-                                    <input class="form-control" id="idNomeUsuario" name="iNomeUsuario" type="text" placeholder="Fulano Silva">
+                                    <input class="form-control" id="idiNomeUsuario" name="iNomeUsuario" type="text" placeholder="Nome Sobrenome">
                                 </div>
                                 <hr class="mt-5">
                             </div>
@@ -55,34 +63,136 @@
                             <div id="1" class="divs">
                                 <hr class="mb-5">  
                                 <div class="input-group mb-3">       
-                                    <label class="input-group-text" for="iCpf">CPF (ou RNE): </label>
-                                    <input class="form-control" id="idCpf" name="iCpf" type="text" placeholder="00011122233">
+                                    <label class="input-group-text" for="iCpf">CPF (ou CRNM): </label>
+                                    <input class="form-control" id="idiCpf" name="iCpf" type="text" placeholder="00011122233">
                                 </div>  
                                 <hr class="mt-5">
                             </div>
 
                             <div id="2" class="divs">
+                                <hr class="mb-5">  
+                                <div class="input-group mb-3">       
+                                    <label class="input-group-text" for="iRfRe">RF/RE: </label>
+                                    <input class="form-control" id="idiRfRe" name="iRfRe" type="text" placeholder="1234567">
+                                </div>  
+                                <hr class="mt-5">
+                            </div>
+
+                            <div id="3" class="divs">
+                                <hr class="mb-5">  
+                                <div class="input-group mb-3">       
+                                    <label class="input-group-text" for="iCoren">COREN: </label>
+                                    <input class="form-control" id="idiCoren" name="iCoren" type="text" placeholder="123456">
+                                </div>  
+                                <hr class="mt-5">
+                            </div>
+
+                            <div id="4" class="divs">
+                                <hr class="mb-5">  
+                                <div class="input-group mb-3">       
+                                    <label class="input-group-text" for="sVinculo">Vínculo: </label>
+                                    <select class="form-select" name="sVinculo" id="idsVinculo">
+                                        <?php $s=mysqli_query($connection,"SELECT * FROM `Vinculo` ORDER BY Vinc");
+                                            while($r = mysqli_fetch_array($s)){ ?>
+                                                <option value="<?php echo $r['Id']?>"><?php echo $r['Vinc'];?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>  
+                                <hr class="mt-5">
+                            </div>
+
+                            <div id="5" class="divs">
+                                <hr class="mb-5">  
+                                <div class="input-group mb-3">       
+                                    <label class="input-group-text" for="sCategoria">Categoria: </label>
+                                    <select class="form-select" name="sCategoria" id="idsCategoria">
+                                        <?php $s=mysqli_query($connection,"SELECT * FROM `Categoria` ORDER BY Cat");
+                                            while($r = mysqli_fetch_array($s)){ ?>
+                                                <option value="<?php echo $r['Id']?>"><?php echo $r['Cat'];?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>  
+                                <hr class="mt-5">
+                            </div>
+
+                            <div id="6" class="divs">
                                 <hr class="mb-5">
                                 <div class="input-group mb-3">
-                                    <label class="input-group-text" for="sTipoUsuario">Tipo de usuário: </label> <!-- select tipousuario -->
+                                    <label class="input-group-text" for="sTipoUsuario">Tipo de usuário: </label>
                                     <select class="form-select" name="sTipoUsuario" id="idsTipoUsuario">
-                                        <?php $s=mysqli_query($connection,"SELECT * FROM `TipoUsuario`");
+                                        <?php $s=mysqli_query($connection,"SELECT * FROM `TipoUsuario` ORDER BY Tipo");
                                             while($r = mysqli_fetch_array($s)){ ?>
-                                                <option value="<?php echo $r['ID']?>"><?php echo $r['Tipo'];?></option>
+                                                <option value="<?php echo $r['Id']?>"><?php echo $r['Tipo'];?></option>
                                         <?php } ?>
                                     </select>
                                 </div>
                                 <hr class="mt-5">
                             </div>
 
-                            <div id="3" class="divs">
+                            <div id="7" class="divs">
                                 <hr class="mb-5">
                                 <div class="input-group mb-3">
-                                    <label class="input-group-text" for="sSetor">Setor: </label> <!-- select setor -->
-                                    <select class="form-select" name="sSetor" id="idsSetor">
-                                        <?php $s=mysqli_query($connection,"SELECT * FROM `Setor`");
+                                    <label class="input-group-text" for="sCondicao">Condição: </label>
+                                    <select class="form-select" name="sCondicao" id="idsCondicao">
+                                        <?php $s=mysqli_query($connection,"SELECT * FROM `Condicao` ORDER BY Cond");
                                             while($r = mysqli_fetch_array($s)){ ?>
-                                                <option value="<?php echo $r['ID']?>"><?php echo $r['Nome'];?></option>
+                                                <option value="<?php echo $r['Id']?>"><?php echo $r['Cond'];?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                                <hr class="mt-5">
+                            </div>
+
+                            <div id="8" class="divs">
+                                <hr class="mb-5">
+                                <div class="input-group mb-3">
+                                    <label class="input-group-text" for="sAcesso">Nível de Acesso: </label>
+                                    <select class="form-select" name="sAcesso" id="idsAcesso">
+                                        <?php $s=mysqli_query($connection,"SELECT * FROM `Acesso` ORDER BY Tipo");
+                                            while($r = mysqli_fetch_array($s)){ ?>
+                                                <option value="<?php echo $r['Id']?>"><?php echo $r['Tipo'];?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                                <hr class="mt-5">
+                            </div>
+
+                            <div id="9" class="divs">
+                                <hr class="mb-5">
+                                <div class="input-group mb-3">
+                                    <label class="input-group-text" for="sBloco">Bloco: </label>
+                                    <select class="form-select" name="sBloco" id="idsBloco">
+                                        <?php $s=mysqli_query($connection,"SELECT * FROM `Bloco` ORDER BY Nome");
+                                            while($r = mysqli_fetch_array($s)){ ?>
+                                                <option value="<?php echo $r['Id']?>"><?php echo $r['Nome'];?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                                <hr class="mt-5">
+                            </div>
+
+                            <div id="10" class="divs">
+                                <hr class="mb-5">
+                                <div class="input-group mb-3">
+                                    <label class="input-group-text" for="sSetor">Setor: </label>
+                                    <select class="form-select" name="sSetor" id="idsSetor">
+                                        <?php $s=mysqli_query($connection,"SELECT * FROM `Setor` ORDER BY Nome");
+                                            while($r = mysqli_fetch_array($s)){ ?>
+                                                <option value="<?php echo $r['Id']?>"><?php echo $r['Nome'];?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                                <hr class="mt-5">
+                            </div>
+
+                            <div id="11" class="divs">
+                                <hr class="mb-5">
+                                <div class="input-group mb-3">
+                                    <label class="input-group-text" for="sTipoEscala">Tipo de Escala: </label>
+                                    <select class="form-select" name="sTipoEscala" id="idsTipoEscala">
+                                        <?php $s=mysqli_query($connection,"SELECT * FROM `TipoEscala`");
+                                            while($r = mysqli_fetch_array($s)){ ?>
+                                                <option value="<?php echo $r['Id']?>"><?php echo $r['Tipo'];?></option>
                                         <?php } ?>
                                     </select>
                                 </div>
